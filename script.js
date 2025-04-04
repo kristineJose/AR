6dcda8262b56eb9e2c3d99d9aa822634e3488aa1
kristineJose <kristin.jose24@gmail.com>
2025-04-04 20:47:30 +0800

    arr

--- script.js
+++ script.js
@@ -1,29 +1,66 @@
 window.onload = () => {
-    const button = document.querySelector('button[data-action="change"]');
-    button.innerText = '﹖';
+    const dropdown = document.getElementById('destination');
+    dropdown.addEventListener('change', () => {
+        const selected = dropdown.value;
+        let place, model;
 
-    let places = staticLoadPlaces();
-    renderPlaces(places);
+        if (selected === 'pokemon') {
+            place = {
+                name: 'Pokémon',
+                location: { lat: 14.698405, lng: 120.978640 }
+            };
+            model = models[0];
+        } else if (selected === 'articuno') {
+            place = {
+                name: 'Articuno',
+                location: { lat: 14.698450, lng: 120.978700 }
+            };
+            model = models[1];
+        } else if (selected === 'dragonite') {
+            place = {
+                name: 'Dragonite',
+                location: { lat: 14.698500, lng: 120.978750 }
+            };
+            model = models[2];
+        }
+
+        renderSelectedPlace(place, model);
+    });
+
+    // Initial trigger
+    dropdown.dispatchEvent(new Event('change'));
 };
 
-function staticLoadPlaces() {
-    return [
-        {
-            name: 'Pokèmon',
-            location: {
-                lat: 14.698405,
-                lng: 120.978640,
-            },
-        },
-    ];
+function renderSelectedPlace(place, model) {
+    const scene = document.querySelector('a-scene');
+    
+    // Remove any old model
+    const oldModel = document.getElementById('ar-model');
+    if (oldModel) oldModel.remove();
+
+    const latitude = place.location.lat;
+    const longitude = place.location.lng;
+
+    const entity = document.createElement('a-entity');
+    entity.setAttribute('id', 'ar-model');
+    entity.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+    entity.setAttribute('gltf-model', model.url);
+    entity.setAttribute('scale', model.scale);
+    entity.setAttribute('rotation', model.rotation);
+    entity.setAttribute('animation-mixer', '');
+
+    scene.appendChild(entity);
+
+    const infoBox = document.querySelector('.instructions');
+    if (infoBox) infoBox.innerText = model.info;
 }
 
 var models = [
     {
         url: './assets/magnemite/scene.gltf',
         scale: '0.5 0.5 0.5',
-        info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 180 0',
+        info: 'Magnemite, Lv. 5, HP 10/10',
     },
     {
         url: './assets/articuno/scene.gltf',
@@ -38,48 +75,3 @@ var models = [
         info: 'Dragonite, Lv. 99, HP 150/150',
     },
 ];
-
-var modelIndex = 0;
-var setModel = function (model, entity) {
-    if (model.scale) {
-        entity.setAttribute('scale', model.scale);
-    }
-
-    if (model.rotation) {
-        entity.setAttribute('rotation', model.rotation);
-    }
-
-    if (model.position) {
-        entity.setAttribute('position', model.position);
-    }
-
-    entity.setAttribute('gltf-model', model.url);
-
-    const div = document.querySelector('.instructions');
-    div.innerText = model.info;
-};
-
-function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
-
-    places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
-
-        let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-
-        setModel(models[modelIndex], model);
-
-        model.setAttribute('animation-mixer', '');
-
-        document.querySelector('button[data-action="change"]').addEventListener('click', function () {
-            var entity = document.querySelector('[gps-entity-place]');
-            modelIndex++;
-            var newIndex = modelIndex % models.length;
-            setModel(models[newIndex], entity);
-        });
-
-        scene.appendChild(model);
-    });
-}
